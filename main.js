d34171a99254eda981d2666bab4021de24be1469
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-11-14 20:21:51 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const houses = [
+  {
+    id: 1,
+    name: 'stark',
+    slogan: 'Winter Is Coming',
+    description: ''
+  },
+  {
+    id: 2,
+    name: 'greyjoy',
+    slogan: 'What Is Dead May Never Die',
+    description: ''
+  },
+  {
+    id: 3,
+    name: 'arryn',
+    slogan: 'As High as Honor',
+    description: ''
+  },
+  {
+    id: 4,
+    name: 'lannister',
+    slogan: 'A Lannister Always Pays His Debts',
+    description: ''
+  },
+  {
+    id: 5,
+    name: 'tully',
+    slogan: 'Family, Duty, Honor',
+    description: ''
+  },
+  {
+    id: 6,
+    name: 'targaryen',
+    slogan: 'Fire and Blood',
+    description: ''
+  },
+  {
+    id: 7,
+    name: 'tyrell',
+    slogan: 'Growing Strong',
+    description: ''
+  },
+  {
+    id: 8,
+    name: 'baratheon',
+    slogan: 'Ours is the Fury',
+    description: ''
+  },
+  {
+    id: 9,
+    name: 'martell',
+    slogan: 'Unbowed, Unbent, Unbroken',
+    description: ''
+  },
+]
+
+// const button = document.querySelector('.menu-item');
+// const content = document.querySelector('.content');
+
+// console.log(button);
+
+// button.addEventListener('click', () => {
+//   content.insertAdjacentHTML('afterbegin', `<div class="slogan">${houses[1].slogan}</div>`)
+// })
